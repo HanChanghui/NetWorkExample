331420611991618ffb2fe15f358f4183f8906a55
chohi1948 <chohi1948@naver.com>
2024-08-29 17:19:23 +0900

    ConnectOkHttp class add

---
+++ app/src/main/java/com/example/networkexample/ConnectOkHttp.kt
@@ -0,0 +1,13 @@
+package com.example.networkexample
+
+import okhttp3.OkHttpClient
+
+class ConnectOkHttp {
+    val client = OkHttpClient()
+    // 동기식
+    fun AsyncConnectHttp() {
+
+    }
+
+    // 비동기식
+}
